622f7ae76cf75e614c20c1e29e87f80c143a5c6d
Gregory.Johnson <3531op@gmail.com>
2020-10-05 09:47:15 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,144 @@
+// function oneToFive() {
+// let i = 1
+//   while (i <= 5) {
+//   console.log(i);
+//   i += 1
+// }
+// }
+// return oneToFive()
+
+
+
+// function challengeTwo(x) {
+// let i = 1
+//   while (i <= x) {
+//     console.log(i);
+//     i += 1
+//   }
+// }
+// return challengeTwo(3)
+
+
+
+// function challengeThree(num1, num2) {
+//   while (num1 !== num2 + 1) {
+//     console.log(num1);
+//     num1 += 1
+//   }
+// }
+// return challengeThree(-2, 1)
+
+
+
+// function challengeFour (num1) {
+// i = 1
+// while(i <= num1) {
+
+//     if (i % 2 === 0) {
+//     console.log(i + " Even")
+// }   else {
+//     console.log(i + " Odd")
+// }  i+=1
+
+// }
+// }
+
+// challengeFour(5)
+
+
+
+// function challengeFive(num1) {
+// let i = 2
+// while (i <= num1) {
+//     console.log(i);
+//     i += 2
+// }
+
+// }
+// challengeFive (10)
+
+
+
+// function challengeSix() {
+//     let i = 10;
+//     while (i > 0) {
+//     console.log(i)
+//         i -- 
+//     }
+// }
+// challengeSix()
+
+
+
+// function challengeSeven() {
+//     let i = 10;
+//     while (i > 0) {
+//         console.log(i);
+//         i--
+//         } if (i = -1 ) {
+//             console.log("Blast off!")
+//         }
+//     }
+
+// challengeSeven()
+
+
+
+// function challengeEight(num) {
+//     while (num > 0) {
+//         console.log(num);
+//         num -= 3
+//     }
+// }
+
+// challengeEight(20)
+
+
+
+// function challengeNine() {
+//     i = 1
+//     while (i < 8) {
+//         console.log (i + "." + " Hello!");
+//         i++
+//     }
+        
+//     }
+
+// challengeNine()
+
+
+// function challengeTen(str) {
+//     str = process.argv[2]
+//     i = 1
+//     while (i < 8) {
+//         console.log(i + ". " + str);
+//         i++;
+//     }     
+// }
+// challengeTen()
+
+
+
+// function challengeEleven(str, numOfTimes) {
+//     str = process.argv[2];
+//     numOfTimes = process.argv[3];
+//     i = 1
+//     while( i <= numOfTimes ) {
+//         console.log(i + ". " + str);
+//         i++;
+//     }
+// }
+// challengeEleven()
+
+
+
+function challengeTwelve(str) {
+    str = process.argv[2];
+    i = 1
+    while ( i <= str.length ) {
+        console.log(str.charAt([0]))
+       i++
+    }      
+}
+challengeTwelve("hello")
+console.log(i);
